6d881bcbadb08fcc8da44ef1c81c92ac3b553053
Paul Broadfoot <prbroadfoot@gmail.com>
2017-10-02 12:49:24 -0500

    set up templating with pug

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
+});
+
+app.listen(3001, function() {
+  console.log('app listening on port 3001!');
+});
