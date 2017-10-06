88491cc9834c1f2c54d34c28185edb0876b4d9e8
Paul Broadfoot <prbroadfoot@gmail.com>
2017-10-06 16:32:41 -0500

    add about page

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+app.get('/about', function(req, res) {
+  res.render('about');
+});
+
 app.get('/frame_list', function(req, res) {
   req.query.hsk_level
     ? renderFrameListForHSKLevel(req, res)
